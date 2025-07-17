3da93e06dfc6f0b0a26b471a089fb520a64ff34d
yeekies85 <cometezero@gmail.com>
2025-07-17 12:45:32 +0800

    0717-XAMPP

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = "DjangoBlog.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+'''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_db",
+        "USER": "django_db",
+        "PASSWORD": ")5ex@eHfxG_krE]d",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
